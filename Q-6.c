30417cf64cdb057a0956f61dcf3440030b271a2c
Meet Vadodariya <163812660+MeetVadodariya@users.noreply.github.com>
2024-04-12 09:17:55 +0530

    Add files via upload

---
+++ Q-6.c
@@ -0,0 +1,23 @@
+//	1 0 1 0 1
+//	1 0 1 0
+//	1 0 1
+//	1 0 
+//	1
+
+
+#include<stdio.h>
+
+main() 
+{
+    int i, j;
+
+    for (i = 5; i >= 1; i--) 
+	{
+        for (j = 1; j <= i; j++) 
+		{
+            printf("%d ", j % 2);
+        }
+        printf("\n");
+    }
+
+}
